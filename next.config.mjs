62ab47ab9b5667195224754b6ba63031cc4751f7
Musca Mihail <muscamihaill@gmail.com>
2024-09-26 12:17:15 +0300

    First commit

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    basePath: process.env.NODE_ENV === "production" ? "/cv-maker" : "",
+    assetPrefix: process.env.NODE_ENV === "production" ? "https://muscaa.github.io/cv-maker/" : "",
+    images: {
+        unoptimized: true,
+    },
+};
+
+export default nextConfig;
