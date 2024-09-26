5ad8f8512878b666b2a2e03b7e82984c1e71f8fd
Musca Mihail <muscamihaill@gmail.com>
2024-09-26 12:33:08 +0300

    Test pdf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 
-import "./globals.css";
+import "./styles/main.css";
 
 const font = Montserrat({ subsets: [ "latin" ] });
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,22 @@ const config: Config = {
     theme: {
         extend: {
             colors: {
-                background: "var(--background)",
-                foreground: "var(--foreground)",
+                "background-1": "var(--background-1)",
+                "background-2": "var(--background-2)",
+                "background-3": "var(--background-3)",
+                "background-4": "var(--background-4)",
+                "text-1": "var(--text-1)",
+                "text-2": "var(--text-2)",
+                "text-3": "var(--text-3)",
+                "text-4": "var(--text-4)",
+                "primary": "var(--primary)",
+                "primary-light": "var(--primary-light)",
+                "secondary": "var(--secondary)",
+                "secondary-light": "var(--secondary-light)",
+                "shadow": "var(--shadow)",
+            },
+            fontSize: {
+                "xxs": [ "0.625rem", "0.75rem" ],
             },
         },
     },
