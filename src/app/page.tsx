4fedb885f8cff5eacdfbc954cacb1cca3f41e700
Musca Mihail <muscamihaill@gmail.com>
2024-10-05 20:14:25 +0300

    Remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import Button from "./components/Button";
 //import * as Utils from "./Utils";
 
 import React from "react";
-import { useEffect } from "react";
 
 /*import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
 import dynamic from "next/dynamic";
