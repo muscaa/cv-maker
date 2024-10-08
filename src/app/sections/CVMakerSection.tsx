3535929247c5e146938102ac71259c1f0b73dc63
Musca Mihail <muscamihaill@gmail.com>
2024-10-08 12:19:26 +0300

    Test buttons

--- src/app/sections/CVMakerSection.tsx
+++ src/app/sections/CVMakerSection.tsx
@@ -2,6 +2,7 @@ import * as Utils from "../Utils";
 import Button from "../components/Button";
 
 export default function CVMakerSection() {
+    const buttons = [ "Test 1", "Test 2", "Test 3" ];
     return (
         <section className="flex flex-col gap-2">
             <Button text="Load Template Zip" onClick={async () => {
@@ -12,6 +13,9 @@ export default function CVMakerSection() {
 
                 console.log(script);
             }} />
+            {buttons.map((text, index) => (
+                <Button key={index} text={text} />
+            ))}
         </section>
     );
 }
